fafa7987f1e0fc65c08715529f8abfe77828dd48
Jie Chen <467116811@qq.com>
2019-04-17 09:55:48 +0800

    新增：创建项目

---
+++ glib-json/glib-json/config.h
@@ -0,0 +1,17 @@
+//
+//  config.h
+//  glib-json-ios
+//
+//  Created by Chen Jie on 2019/4/16.
+//  Copyright © 2019 Chen Jie. All rights reserved.
+//
+
+#ifndef config_h
+#define config_h
+
+#include "json-glib.h"
+
+#define GETTEXT_PACKAGE "GETTEXT_PACKAGE"
+#define JSON_LOCALEDIR "JSON_LOCALEDIR"
+
+#endif /* config_h */
